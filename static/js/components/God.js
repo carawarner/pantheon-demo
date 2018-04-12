71117eac6c2e78df25b64d235514b62b3f81f704
carawarner <warnercara@gmail.com>
2018-04-12 13:26:07 -0400

    Add prop validation on God

--- static/js/components/God.js
+++ static/js/components/God.js
@@ -1,9 +1,14 @@
 import React from "react";
+import PropTypes from "prop-types";
 
 export const God = function(props) {
   return (
     <p className="god">
-      <span className="god-name">{props.god.name}</span> <span className="god-epithet">{props.god.epithet}</span>
+      <span className="god-name">{props.god.name}</span> - <span className="god-epithet">{props.god.epithet}</span>
     </p>
   );
+}
+
+God.propTypes = {
+  god: PropTypes.object.isRequired
 }
