85cbd507b382d7267f0ead8e07b7e321ef91eca3
carawarner <warnercara@gmail.com>
2018-04-19 07:30:08 -0400

    Allow styles to be loaded by React app

--- static/js/containers/App.js
+++ static/js/containers/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import PantheonContainer from "./PantheonContainer";
 import { About } from "../components/About";
+import "../../css/main.css";
 
 export default class App extends React.Component {
   render() {
