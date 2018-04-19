f3ab78746b1c8672a67c7026bf0fcfc5275ad2e2
carawarner <warnercara@gmail.com>
2018-04-19 11:57:37 -0400

    Add a names endpoint

--- server/main.py
+++ server/main.py
@@ -1,10 +1,17 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 from pantheon.pantheons import Pantheon
+import pantheon.names as names
+
 app = Flask(__name__, static_folder='../static/dist', template_folder='../static')
 
 @app.route('/', methods=['GET'])
 def index():
     return render_template('index.html')
+
+@app.route('/names', methods=['GET'])
+def fetch_names():
+    results = names.get_ethnicities()
+    return jsonify(names=results)
 
 if __name__ == '__main__':
     app.run()
