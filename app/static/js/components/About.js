746ab1e3c72460b097938dc944d3fa097eaf5591
carawarner <warnercara@gmail.com>
2018-05-06 17:23:27 -0400

    Add additional explanatory text and styling

--- app/static/js/components/About.js
+++ app/static/js/components/About.js
@@ -4,12 +4,24 @@ export const About = function(props) {
   return (
     <div className="text-is-centered">
       <h1 className="title">Pantheon Generator</h1>
-      <p className="description-text">
-        This is a demo of the{" "}
+      <p className="description">
+        This is a{" "}
+        <a className="link" href="https://github.com/carawarner/pantheon-demo">
+          beta demo
+        </a>{" "}
+        of the{" "}
         <a className="link" href="https://github.com/carawarner/pantheon">
           Pantheon Generator
         </a>{" "}
-        library.
+        library. It uses sexual reproduction as a metaphor to procedurally
+        generate deities that feel related.
+      </p>
+      <p>
+        <span class="is-warning">
+          WARNING: beta has NO form validation and NO loading spinner.{" "}
+        </span>{" "}
+        Fill everything in, provide an egg donor and a sperm donor, and be
+        patient. Generation takes several seconds.
       </p>
     </div>
   );
