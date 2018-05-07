c73173b0883600aed877ada5aa3ae0305e91bfe6
carawarner <warnercara@gmail.com>
2018-05-07 11:36:30 -0400

    Add qualifier to results when set is small

--- app/static/js/components/Pantheon.js
+++ app/static/js/components/Pantheon.js
@@ -5,14 +5,26 @@ import { God } from "./God";
 export const Pantheon = function(props) {
   return (
     <div className="text-is-centered">
-      <h3 className="title">
-        {props.gods.length > 0 ? "Generated Deities" : ""}
-      </h3>
+      {props.gods.length > 0 && (
+        <h3 className="title">Generated {props.gods.length} Deities</h3>
+      )}
+
       {props.gods.map((god, i) => (
         <div key={"god" + i}>
           <God god={god} />
         </div>
       ))}
+
+      {props.gods.length > 0 &&
+        props.gods.length < 12 && (
+          <p className="qualifier">
+            This Pantheon is small because early generations were XY-heavy. See
+            why in the note about{" "}
+            <a href="https://github.com/carawarner/pantheon#code-imitating-nature">
+              Sex
+            </a>.
+          </p>
+        )}
     </div>
   );
 };
